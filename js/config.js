aad00b91ff32261d565a09034650526b40cea4e3
MistyMoo <alisonparry@hotmail.com>
2024-11-26 21:32:22 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_sh50UQ74s', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1p2drqbnh5d1gn8hag1op4pmqt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '33a2l6i12841rijipkrdmpfjkt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
